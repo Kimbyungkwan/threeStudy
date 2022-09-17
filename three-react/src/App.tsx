08612b2c8934f0e592dcec4e6be231020fde3b25
Kimbyungkwan <kqudk@naver.com>
2022-09-17 23:35:48 +0900

    테스트 상자

--- three-react/src/App.tsx
+++ three-react/src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import { Canvas } from '@react-three/fiber';
+import { Cloud, OrbitControls, Stars } from '@react-three/drei';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Canvas style={{ background: '#000' }}>
+        <OrbitControls />
+        {/* <Stars /> */}
+        <Stars />
+        <Cloud position={[0, 0, 0]} speed={0.2} opacity={0.3} />
+        <Cloud position={[5, 2.5, -5]} speed={0.2} opacity={0.3} />
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 15, 10]} angle={0.3} />
+        <mesh position={[0, 0, 0]}>
+          <boxBufferGeometry attatch='geometry' />
+          <meshLambertMaterial attatch='material' color='orange' />
+        </mesh>
+      </Canvas>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
